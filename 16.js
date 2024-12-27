9d21021c1263106e7f5e978771e3e41fc6801691
Raju_0005 <rajugbp2006@gmail.com>
2024-12-27 17:55:10 +0530

    Add files via upload

---
+++ 16.js
@@ -0,0 +1,24 @@
+// CHAPTER-5 ARRAYS
+// Arrays=Collection of items
+let class_12 = [88,78,79,90,56,73]
+// class_12[6] = 99//Adding the new number
+// class_12[0] = 89//Changing the number
+// console.log(class_12[0])
+// console.log(class_12[1])
+// console.log(class_12[2])
+// console.log(class_12[3])
+// console.log(class_12[4])
+// console.log(class_12[5])
+// console.log(class_12[6])
+// console.log(typeof class_12)
+// console.log(clengthlass_12.[i])
+
+// Quik Quize=>
+// for(let i=0;i<6;i++){
+// 	console.log(class_12[i])
+// }
+
+
+for(let i=0;i<class_12.length;i++){
+	console.log(class_12[i])
+}
---
+++ 17.js
@@ -0,0 +1,18 @@
+// ARRAYS METHODS
+let num = [45,98,67,99,9]
+// let a = num.toString()//[ek array ko string m badlane ke liye]
+// console.log(a, typeof a)
+// let b = num.join(" & ")//[array m kuch bhi add krane ke liye]
+// console.log(b)
+// num.pop()//[array ke last element ko nikalne ke liye]
+// console.log(num)
+// let c = num.pop()//[array ke last element ko return krne ke liye]
+// console.log(num, c)
+// let d = num.push(55)//[array me new element add krne ke liye]
+// console.log(num, d)
+// let f = num.shift()
+// console.log(f)//[array ke first element ko print krane ke liye]
+// let e = num.shift()
+// console.log(e,num)//[array ke first element ko remove krne k liye]
+let f = num.unshift(7)
+console.log(f,num)//[array m first element add krne ke liye or returns to new array lenght]
